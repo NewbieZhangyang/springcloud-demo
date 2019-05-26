1f188517ae47d8530d9976598b403e8083fe0470
Newbie Zhangyang <yy810896908@163.com>
2019-05-26 14:26:13 +0800

    修改UserConftoller的接口： 1、增加日志输出 2、在响应的json数据中，增加请求的url信息

--- provider-user/src/main/java/com/newbie/provideruser/controller/UserController.java
+++ provider-user/src/main/java/com/newbie/provideruser/controller/UserController.java
@@ -1,23 +1,32 @@
 package com.newbie.provideruser.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.newbie.provideruser.domain.User;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    Log log = LogFactory.getLog(this.getClass());
 
     @RequestMapping("/getUserInfo")
-    public String getUserInfo(User user){
+    public String getUserInfo(User user, HttpServletRequest request){
         String message = "用户名：";
         if(user == null || user.getUsername() == null){
             message += "简小六";
         }else {
             message += user.getUsername();
         }
+        String url = " 请求url = "+request.getRequestURL();
+        log.info(message);
+        log.info(url);
+        message += "  ,  "+url;
         return message;
     }
 }
