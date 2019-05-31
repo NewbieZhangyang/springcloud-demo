92bbe7430e2c8074751b3e902b846779229c71c9
Newbie Zhangyang <yy810896908@163.com>
2019-05-31 13:25:24 +0800

    使用spring cloud config组件，实现配置文件的管理 1、config-server : 配置文件服务中心 2、config-client : 从config-server中读取配置文件参数数据

---
+++ config-client/src/main/java/com/newbie/configclient/controller/ConfigController.java
@@ -0,0 +1,33 @@
+package com.newbie.configclient.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    Log log = LogFactory.getLog(this.getClass());
+
+    @Value("${foo}")
+    String foo;
+
+    @Value("${username}")
+    String username;
+
+    @Value("${title}")
+    String title;
+
+    @RequestMapping(value = "/config")
+    public String getConfigInfo(){
+        String message = "配置中心客户端，从服务端获取参数: ";
+        message += " foo = "+foo;
+        message += " ,username = "+username;
+        message += " , title = "+title;
+        log.info(message);
+        return message;
+    }
+
+}
